bfe5fe27cc4be747446912c6a52a02482d41b50c
RanMasterApps-collab <testing2024user@gmail.com>
2026-05-17 13:20:14 +0500

    Create SplashScreen.kt

---
+++ screens/SplashScreen.kt
@@ -0,0 +1,56 @@
+@Composable
+fun Mp4SplashScreen(navController: NavController) {
+
+    val context = LocalContext.current
+
+    val exoPlayer = remember {
+
+        ExoPlayer.Builder(context).build().apply {
+
+            val mediaItem = MediaItem.fromUri(
+                Uri.parse("android.resource://${context.packageName}/${R.raw.splash_canva_anim}")
+            )
+
+            setMediaItem(mediaItem)
+
+            prepare()
+
+            playWhenReady = true
+        }
+    }
+
+    // ✅ 3 sec baad navigate
+    LaunchedEffect(Unit) {
+
+        delay(4000)
+
+        navController.navigate("login") {
+
+            popUpTo("splash") {
+                inclusive = true
+            }
+        }
+    }
+
+    // ✅ Release player
+    DisposableEffect(Unit) {
+
+        onDispose {
+            exoPlayer.release()
+        }
+    }
+
+    AndroidView(
+        factory = {
+            PlayerView(it).apply {
+
+                player = exoPlayer
+
+                useController = false
+
+                resizeMode = AspectRatioFrameLayout.RESIZE_MODE_ZOOM
+            }
+        },
+        modifier = Modifier.fillMaxSize()
+    )
+}

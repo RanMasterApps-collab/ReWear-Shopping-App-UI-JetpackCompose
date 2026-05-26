feab9c68411d9944982eea986f3bcb2385b37ac8
RanMasterApps-collab <testing2024user@gmail.com>
2026-05-26 18:26:50 +0500

    Create CategoryItem.kt

---
+++ components/CategoryItem.kt
@@ -0,0 +1,34 @@
+@Composable
+fun CategoryItem(
+    icon: ImageVector,
+    title: String,
+    goldColor: Color
+) {
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Box(
+            modifier = Modifier
+                .size(70.dp)
+                .clip(CircleShape)
+                .background(Color.White.copy(alpha = 0.10f)),
+            contentAlignment = Alignment.Center
+        ) {
+             Icon(
+                imageVector = icon,
+                contentDescription = null,
+                tint = goldColor,
+                modifier = Modifier.size(30.dp)
+            )
+        }
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        Text(
+            text = title,
+            color = Color.White
+        )
+    }
+}
